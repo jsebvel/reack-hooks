752405ee7dfe52d535adad2a5ecb95b328769815
Sebastián velásquez <jsebvel@gmail.com>
2024-02-17 23:37:36 -0500

    feat: add navbar

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,22 +3,18 @@ import { Link, Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { AboutPage } from "./AboutPage";
+import { Navbar } from "./Navbar";
 
 export const MainApp = () => {
   return (
     <div>
-      <h1>MainApp!</h1>
-    <Link to="/">Home</Link>
-    <Link to="/login">Login</Link>
-    <Link to="/about">About</Link>
-
-
+      <Navbar />
       <hr />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="about" element={<AboutPage />} />
-        <Route path="/*" element={<Navigate to="about" />}/>
+        <Route path="/*" element={<Navigate to="about" />} />
       </Routes>
     </div>
   );
